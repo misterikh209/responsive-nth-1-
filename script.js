864afbacbd491384f097c3e975a446e4308eb6f3
misterikh209 <saitovixtiyor209@gmail.com>
2024-02-16 18:02:43 +0500

    responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn")
+let menu = document.getElementById("menu")
+
+btn.addEventListener("click",()=> {
+     menu.classList.toggle("active")
+     btn.style.color == "white"
+ })
